a3d8539ff5a5538b8fadcff9dd8a1607016cd14e
Felipe Mateus Falcão Barreto <126531887+felipemfb@users.noreply.github.com>
2025-12-15 09:26:50 +0000

    Uploading VM Front files

---
+++ Virtualization/MVFront/front/script.js
@@ -0,0 +1,60 @@
+var appElement = document.querySelector("#app"); 
+
+var buttonElement = document.createElement("button"); 
+var inputElement = document.createElement("input"); 
+var listElement = document.createElement("ul"); 
+
+buttonElement.appendChild(document.createTextNode("Add")); 
+
+appElement.appendChild(inputElement);
+appElement.appendChild(buttonElement);
+appElement.appendChild(listElement);
+
+buttonElement.onclick = function(){
+    add();
+}
+
+inputElement.setAttribute('id', 'name');
+inputElement.setAttribute('type', 'text');
+listElement.setAttribute('id', 'list');
+const BACKEND_IP = "192.168.56.20";
+
+async function add(){
+    var savedName = inputElement.value;
+
+    if (savedName.trim() === ""){
+        return;
+    }
+
+    try {
+        const res = await fetch(`http://${BACKEND_IP}:3000/items`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ text: savedName })
+        });
+        
+        if (!res.ok) throw new Error("Error connecting to database");
+
+        const itemListLine = document.createElement('li');
+        itemListLine.textContent = savedName;
+        listElement.appendChild(itemListLine);
+
+        inputElement.value = "";
+    } catch (e) {
+        console.error("Error by adding an item:", e);
+    }
+}
+
+async function load(){
+    listElement.innerHTML = "";
+    const res = await fetch(`http://${BACKEND_IP}:3000/items`);
+    const items = await res.json();
+
+    items.forEach(item => {
+        var itemListLine = document.createElement('li');
+        itemListLine.textContent = item.text;
+        listElement.appendChild(itemListLine); 
+    })
+}
+
+load();
